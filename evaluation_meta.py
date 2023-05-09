fc92a575054c9d544187d62637038c594f984147
hhc1997 <46480312+hhc1997@users.noreply.github.com>
2023-05-09 10:54:17 +0800

    Update evaluation_meta.py

--- evaluation_meta.py
+++ evaluation_meta.py
@@ -2,19 +2,13 @@
 
 from __future__ import print_function
 import os
-import sys
 import time
-import json
-from itertools import chain
-
 import torch
 import numpy as np
-
 from vocab import Vocabulary, deserialize_vocab
 from models import SGRAF,Meta_Sim
 from utils import AverageMeter, ProgressMeter
 from data import get_dataset, get_loader
-
 
 def encode_data(model, data_loader, log_step=10, logging=print):
     """Encode all images and captions loadable by `data_loader`
@@ -68,118 +62,6 @@ def encode_data(model, data_loader, log_step=10, logging=print):
     # return img_embs, cap_embs, cap_lens, image_ids
     return img_embs, cap_embs, cap_lens
 
-def validate(opt, val_loader, models = [],meta_nets =[]):
-    # compute the encoding for all the validation images and captions
-    if opt.data_name == "cc152k_precomp":
-        per_captions = 1
-    elif opt.data_name in ["coco_precomp", "f30k_precomp"]:
-        per_captions = 5
-    sims_mean = 0
-    count = 0
-    for ind in range(len(models)):
-        count += 1
-        print("Encoding with model {}".format(ind))
-        img_embs, cap_embs, cap_lens = encode_data(
-            models[ind], val_loader, opt.log_step
-        )
-
-        # clear duplicate 5*images and keep 1*images FIXME
-        img_embs = np.array(
-            [img_embs[i] for i in range(0, len(img_embs), per_captions)]
-        )
-
-        # record computation time of validation
-        start = time.time()
-        print("Computing similarity from model {}".format(ind))
-        sims_mean += shard_attn_scores(
-            models[ind], img_embs, cap_embs, cap_lens,meta_nets[ind],opt, shard_size=100
-        )
-        end = time.time()
-        print(
-            "Calculate similarity time with model {}: {:.2f} s".format(ind, end - start)
-        )
-
-    # average the sims
-    sims_mean = sims_mean / count
-
-
-
-
-
-    # caption retrieval
-    (r1, r5, r10, medr, meanr) = i2t(img_embs.shape[0], sims_mean, per_captions)
-    print(
-        "Image to text: {:.1f}, {:.1f}, {:.1f}, {:.1f}, {:.1f}".format(
-            r1, r5, r10, medr, meanr
-        )
-    )
-
-    # image retrieval
-    (r1i, r5i, r10i, medri, meanr) = t2i(img_embs.shape[0], sims_mean, per_captions)
-    print(
-        "Text to image: {:.1f}, {:.1f}, {:.1f}, {:.1f}, {:.1f}".format(
-            r1i, r5i, r10i, medri, meanr
-        )
-    )
-
-    # sum of recalls to be used for early stopping
-    # r_sum = r1 + 0.5*r5 + 0.1*r10 + r1i + 0.5*r5i + 0.1*r10i
-
-    return r1 ,r5 ,r10 , r1i ,r5i ,r10i
-
-def encode_data_test(model, data_loader, log_step=10, logging=print):
-    """Encode all images and captions loadable by `data_loader`
-    """
-    batch_time = AverageMeter("batch", ":6.3f")
-    data_time = AverageMeter("data", ":6.3f")
-    progress = ProgressMeter(len(data_loader), [batch_time, data_time], prefix="Encode")
-
-    # switch to evaluate mode
-    model.eval()
-    # np array to keep all the embeddings
-    img_embs = None
-    cap_embs = None
-
-    # max text length
-    max_n_word = 0
-    for i, (images, captions, lengths, ids) in enumerate(data_loader):
-        max_n_word = max(max_n_word, max(lengths))
-
-    image_ids = []
-    end = time.time()
-    for i, (images, captions, lengths, ids) in enumerate(data_loader):
-        data_time.update(time.time() - end)
-        # image_ids.extend(img_ids)
-        images, captions = images.cuda(), captions.cuda()
-        # compute the embeddings
-        with torch.no_grad():
-            img_emb, cap_emb, cap_len = model.forward_emb(images, captions, lengths)
-        if img_embs is None:
-            img_embs = np.zeros(
-                (len(data_loader.dataset), img_emb.size(1), img_emb.size(2))
-            )
-            cap_embs = np.zeros((len(data_loader.dataset), max_n_word, cap_emb.size(2)))
-            cap_lens = [0] * len(data_loader.dataset)
-        # cache embeddings
-        img_embs[ids] = img_emb.data.cpu().numpy().copy()
-        cap_embs[ids, : max(lengths), :] = cap_emb.data.cpu().numpy().copy()
-
-        for j, nid in enumerate(ids):
-            cap_lens[nid] = cap_len[j]
-
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        if i % log_step == 0:
-            progress.display(i)
-
-        del images, captions
-    # return img_embs, cap_embs, cap_lens, image_ids
-    return img_embs, cap_embs, cap_lens
-
-
-
 def evalrank(model_path, data_path=None, vocab_path=None, split="dev", fold5=False):
     """
     Evaluate a trained model on either dev or test. If `fold5=True`, 5 fold
@@ -232,8 +114,113 @@ def evalrank(model_path, data_path=None, vocab_path=None, split="dev", fold5=Fal
 
 
     print("Computing results...")
-    validate(opt,data_loader,[net_A,net_B],[meta_net_A,meta_net_B])
+    with torch.no_grad():
+        img_embs_A, cap_embs_A, cap_lens_A = encode_data(net_A, data_loader)
+        img_embs_B, cap_embs_B, cap_lens_B = encode_data(net_B, data_loader)
+    print(
+        "Images: %d, Captions: %d"
+        % (img_embs_A.shape[0] / per_captions, cap_embs_A.shape[0])
+    )
+    if not fold5:
+        # no cross-validation, full evaluation FIXME
+        img_embs_A = np.array(
+            [img_embs_A[i] for i in range(0, len(img_embs_A), per_captions)]
+        )
+        img_embs_B = np.array(
+            [img_embs_B[i] for i in range(0, len(img_embs_B), per_captions)]
+        )
 
+        # record computation time of validation
+        start = time.time()
+        sims_A = shard_attn_scores(
+            net_A, img_embs_A, cap_embs_A, cap_lens_A, meta_net_A, opt, shard_size=1000
+        )
+        sims_B = shard_attn_scores(
+            net_B, img_embs_B, cap_embs_B, cap_lens_B, meta_net_B, opt, shard_size=1000
+        )
+        sims = (sims_A + sims_B) / 2
+        end = time.time()
+        print("calculate similarity time:", end - start)
+
+        # bi-directional retrieval
+        # caption retrieval
+        (r1, r5, r10, medr, meanr) = i2t(img_embs_A.shape[0], sims, per_captions)
+        print(
+            "Image to text: {:.1f}, {:.1f}, {:.1f}, {:.1f}, {:.1f}".format(
+                r1, r5, r10, medr, meanr
+            )
+        )
+
+        # image retrieval
+        (r1i, r5i, r10i, medri, meanr) = t2i(img_embs_A.shape[0], sims, per_captions)
+        print(
+            "Text to image: {:.1f}, {:.1f}, {:.1f}, {:.1f}, {:.1f}".format(
+                r1i, r5i, r10i, medri, meanr
+            )
+        )
+    else:
+        # 5fold cross-validation, only for MSCOCO
+        results = []
+        for i in range(5):
+            # 5fold split
+            img_embs_shard_A = img_embs_A[i * 5000 : (i + 1) * 5000 : 5]
+            cap_embs_shard_A = cap_embs_A[i * 5000 : (i + 1) * 5000]
+            cap_lens_shard_A = cap_lens_A[i * 5000 : (i + 1) * 5000]
+
+            img_embs_shard_B = img_embs_B[i * 5000 : (i + 1) * 5000 : 5]
+            cap_embs_shard_B = cap_embs_B[i * 5000 : (i + 1) * 5000]
+            cap_lens_shard_B = cap_lens_B[i * 5000 : (i + 1) * 5000]
+
+            start = time.time()
+            sims_A = shard_attn_scores(
+                net_A,
+                img_embs_shard_A,
+                cap_embs_shard_A,
+                cap_lens_shard_A,
+                meta_net_A,
+                opt,
+                shard_size=1000,
+            )
+            sims_B = shard_attn_scores(
+                net_B,
+                img_embs_shard_B,
+                cap_embs_shard_B,
+                cap_lens_shard_B,
+                meta_net_B,
+                opt,
+                shard_size=1000,
+            )
+            sims = (sims_A + sims_B) / 2
+            end = time.time()
+            print("calculate similarity time:", end - start)
+
+            r, rt0 = i2t(
+                img_embs_shard_A.shape[0], sims, per_captions=5, return_ranks=True
+            )
+            print("Image to text: %.1f, %.1f, %.1f, %.1f, %.1f" % r)
+            ri, rti0 = t2i(
+                img_embs_shard_A.shape[0], sims, per_captions=5, return_ranks=True
+            )
+            print("Text to image: %.1f, %.1f, %.1f, %.1f, %.1f" % ri)
+
+            if i == 0:
+                rt, rti = rt0, rti0
+            ar = (r[0] + r[1] + r[2]) / 3
+            ari = (ri[0] + ri[1] + ri[2]) / 3
+            rsum = r[0] + r[1] + r[2] + ri[0] + ri[1] + ri[2]
+            print("rsum: %.1f ar: %.1f ari: %.1f" % (rsum, ar, ari))
+            results += [list(r) + list(ri) + [ar, ari, rsum]]
+
+        print("-----------------------------------")
+        print("Mean metrics: ")
+        mean_metrics = tuple(np.array(results).mean(axis=0).flatten())
+        print("rsum: %.1f" % (mean_metrics[10] * 6))
+        mean_i2t = (mean_metrics[0] + mean_metrics[1] + mean_metrics[2]) / 3
+        print("Average i2t Recall: %.1f" % mean_i2t)
+        print("Image to text: %.1f %.1f %.1f %.1f %.1f" % mean_metrics[:5])
+        mean_t2i = (mean_metrics[5] + mean_metrics[6] + mean_metrics[7]) / 3
+        print("Average t2i Recall: %.1f" % mean_t2i)
+        print("Text to image: %.1f %.1f %.1f %.1f %.1f" % mean_metrics[5:10])
 
 def shard_attn_scores(model, img_embs, cap_embs, cap_lens,meta_net,opt, shard_size=1000):
     n_im_shard = (len(img_embs) - 1) // shard_size + 1
@@ -251,26 +238,6 @@ def shard_attn_scores(model, img_embs, cap_embs, cap_lens,meta_net,opt, shard_si
                 l = cap_lens[ca_start:ca_end]
                 #sim,_ = model.module.forward_sim(im, ca, l,meta_net)
                 sim, _ = model.forward_sim(im, ca, l, meta_net)
-
-            sims[im_start:im_end, ca_start:ca_end] = sim.data.cpu().numpy()
-    return sims
-
-
-def shard_attn_scores_test(model,meta_net, img_embs, cap_embs, cap_lens, opt, shard_size=1000):
-    n_im_shard = (len(img_embs) - 1) // shard_size + 1
-    n_cap_shard = (len(cap_embs) - 1) // shard_size + 1
-
-    sims = np.zeros((len(img_embs), len(cap_embs)))
-    for i in range(n_im_shard):
-        im_start, im_end = shard_size * i, min(shard_size * (i + 1), len(img_embs))
-        for j in range(n_cap_shard):
-            ca_start, ca_end = shard_size * j, min(shard_size * (j + 1), len(cap_embs))
-
-            with torch.no_grad():
-                im = torch.from_numpy(img_embs[im_start:im_end]).float().cuda()
-                ca = torch.from_numpy(cap_embs[ca_start:ca_end]).float().cuda()
-                l = cap_lens[ca_start:ca_end]
-                sim,_ = model.forward_sim(im, ca, l,meta_net)
 
             sims[im_start:im_end, ca_start:ca_end] = sim.data.cpu().numpy()
     return sims
@@ -311,7 +278,6 @@ def i2t(npts, sims, per_captions=1, return_ranks=False):
     else:
         return (r1, r5, r10, medr, meanr)
 
-
 def t2i(npts, sims, per_captions=1, return_ranks=False):
     """
     Text->Images (Image Search)
@@ -345,5 +311,8 @@ def t2i(npts, sims, per_captions=1, return_ranks=False):
         return (r1, r5, r10, medr, meanr), (ranks, top1, top5, retreivaled_index)
     else:
         return (r1, r5, r10, medr, meanr)
+
+
+
 
 
